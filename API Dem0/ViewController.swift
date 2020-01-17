0d617a64bb5d29744c81b860cf84d3f55eb42fd7
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-17 16:05:24 -0500

    initial commit

--- API Dem0/ViewController.swift
+++ API Dem0/ViewController.swift
@@ -12,6 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0637c9d91efa3d498e8fda2f4540e4f4")!
+        
+        let task = URLSession.shared.dataTask(with: url)
+        {
+            (data,response,error) in
+            if let errro = error
+            {
+                print(error)
+            }
+            else
+            {
+                if let urlContent = data
+                {
+                    do{
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as?  NSDictionary
+                        print(jsonResult)
+                        
+                        
+                    }catch
+                    {
+                        print(error)
+                    }
+                }
+            }
+        }
+        //start the task
+        task.resume()
+        
         // Do any additional setup after loading the view.
     }
 
