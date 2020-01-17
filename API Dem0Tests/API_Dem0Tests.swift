ef1a111099c14ce6c8e958d414b87cd776fcbed5
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-17 15:19:26 -0500

    Initial Commit

---
+++ API Dem0Tests/API_Dem0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  API_Dem0Tests.swift
+//  API Dem0Tests
+//
+//  Created by MacStudent on 2020-01-17.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import API_Dem0
+
+class API_Dem0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
